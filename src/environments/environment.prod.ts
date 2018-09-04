b89b9a967e63b27c329b843d0a09cc22259ff782
Vasary <gievoi.v@gmail.com>
2018-09-04 23:06:10 +0300

    Fix core address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,9 @@
 import * as data from '../../package.json';
-import * as settings from '../../.environment.json';
 
 export const environment = {
     production: true,
 
-    core: 'http://localhost:81',
+    core: 'http://46.21.249.32',
 
     version: data.version,
     name: data.name,
